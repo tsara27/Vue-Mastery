1fdf8de843cfff7ef04656de1a59e1dd66dd5c40
Tsara Sudrajat <tsara.tf@gmail.com>
2021-03-08 14:57:28 +0700

    Enhance design

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
       image: './assets/images/socks_green.jpg',
       url: 'https://google.com',
       description: 'This is the description of the product',
+      details: ['50% cotton', '30% wool', '20% polyester'],
       inStock: true,
       inventory: 8,
       onSale: true
