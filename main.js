afbddae58f9c928f540296f8aa7fc3202d580a53
Tsara Sudrajat <tsara.tf@gmail.com>
2021-03-08 13:39:11 +0700

    Add conditionals

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 const product = 'Socks'
+
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Socks',
+      image: './assets/images/socks_green.jpg',
+      url: 'https://google.com',
+      description: 'This is the description of the product',
+      inStock: true,
+      inventory: 8,
+      onSale: true
+    }
+  }
+})
